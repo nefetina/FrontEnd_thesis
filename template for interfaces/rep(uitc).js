d99978154c01051d66f649ee0a1fbd225e08b6e2
M1xels <micoryan.sanpablo@gsfe.tupcavite.edu.ph>
2022-07-30 01:55:22 +0800

    inayos ko reports oks na repsonsive na lang siguro then permission meron inuupdate ko na rin itsura ng homepage ng student at faculty records na lang kulang sa uitc.

--- template for interfaces/rep(uitc).js
+++ template for interfaces/rep(uitc).js
@@ -23,26 +23,213 @@ function closeNav() {
 
 }
 
+/*breadcrumb buttons navigation*////////////////
+function requestforrep(){
+    var a = document.getElementById("tableform1");
+    var b = document.getElementById("tableform2");
+    var c = document.getElementById("tableform3");
+    var d = document.getElementById("tableform4");
+    var e = document.getElementById("tableform5");
+    var f = document.getElementById("tableform6");
+    
 
-function next() {
+    var y = document.getElementById("table1");
+    var x = document.getElementById("table2");
+    var z = document.getElementById("table3");
+
+
+
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+
+    
+  
+}
+function maintenance() {
+    var a = document.getElementById("tableform1");
+    var b = document.getElementById("tableform2");
+    var c = document.getElementById("tableform3");
+    var d = document.getElementById("tableform4");
+    var e = document.getElementById("tableform5");
+    var f = document.getElementById("tableform6");
+
+    var y = document.getElementById("table1");
+    var x = document.getElementById("table2");
+    var z = document.getElementById("table3");
+
+    y.style.display = "none";
+    x.style.display = "block";
+    z.style.display = "none";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+  
+}
+function borrow(){
+    var a = document.getElementById("tableform1");
+    var b = document.getElementById("tableform2");
+    var c = document.getElementById("tableform3");
+    var d = document.getElementById("tableform4");
+    var e = document.getElementById("tableform5");
+    var f = document.getElementById("tableform6");
+
+    var y = document.getElementById("table1");
+    var x = document.getElementById("table2");
+    var z = document.getElementById("table3");
+    
+    y.style.display = "none";
+    x.style.display = "none";
+    z.style.display = "block";
+  
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+}
+
+
+
+/*tableform 1.1-1.3 navigation*////////////////
+
+function next1() {
     var y = document.getElementById("tableform1");
     var x = document.getElementById("tableform2");
+    var z = document.getElementById("tableform3");
     
     y.style.display = "none";
     x.style.display = "block";
+    z.style.display = "none";
     
 
     
   
 }
-function back() {
+
+
+function next2() {
     var y = document.getElementById("tableform1");
     var x = document.getElementById("tableform2");
+    var z = document.getElementById("tableform3");
     
-    y.style.display = "block";
+    y.style.display = "none";
     x.style.display = "none";
+    z.style.display = "block";
     
 
     
   
+}
+function back1() {
+    var y = document.getElementById("tableform1");
+    var x = document.getElementById("tableform2");
+    var z = document.getElementById("tableform3");
+
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+
+    
+  
+}
+
+function back2() {
+    var y = document.getElementById("tableform1");
+    var x = document.getElementById("tableform2");
+    var z = document.getElementById("tableform3");
+
+    y.style.display = "none";
+    x.style.display = "block";
+    z.style.display = "none";
+
+    
+  
+}
+
+
+
+
+/*tableform 2.1-2.3 navigation*////////////////
+
+function next3() {
+    var y = document.getElementById("tableform4");
+    var x = document.getElementById("tableform5");
+    var z = document.getElementById("tableform6");
+    
+    y.style.display = "none";
+    x.style.display = "block";
+    z.style.display = "none";
+    
+
+    
+  
+}
+
+
+function next4() {
+    var y = document.getElementById("tableform4");
+    var x = document.getElementById("tableform5");
+    var z = document.getElementById("tableform6");
+    
+    y.style.display = "none";
+    x.style.display = "none";
+    z.style.display = "block";
+    
+
+    
+  
+}
+function back3() {
+    var y = document.getElementById("tableform4");
+    var x = document.getElementById("tableform5");
+    var z = document.getElementById("tableform6");
+
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+
+    
+  
+}
+
+function back4() {
+    var y = document.getElementById("tableform4");
+    var x = document.getElementById("tableform5");
+    var z = document.getElementById("tableform6");
+
+    y.style.display = "none";
+    x.style.display = "block";
+    z.style.display = "none";
+
+    
+  
+}
+
+
+/*opening the form from tables*////////////////
+function form11(){
+    var y = document.getElementById("tableform1");
+    var x = document.getElementById("table1");
+    y.style.display = "block";
+    x.style.display = "none";
+}
+
+function form21(){
+    var y = document.getElementById("tableform4");
+    var x = document.getElementById("table2");
+    y.style.display = "block";
+    x.style.display = "none";
 }
