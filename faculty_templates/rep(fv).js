cfab6cc373988f5fb56657a902639c9bcd9a213e
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2022-08-03 19:03:03 +0800

    Add Files

---
+++ faculty_templates/rep(fv).js
@@ -0,0 +1,43 @@
+function openNav() {
+
+    var y = document.getElementById("mySidebar");
+    var x = document.getElementById("open");
+    var z = document.getElementById("close");
+
+    y.style.display = "block";
+    x.style.display = "block";
+    z.style.display = "block";
+
+
+}
+
+function closeNav() {
+
+    var y = document.getElementById("mySidebar");
+    var x = document.getElementById("close");
+    var z = document.getElementById("open");
+
+    y.style.display = "none";
+    x.style.display = "block";
+    z.style.display = "block";
+
+}
+/*breadcrumb buttons navigation*/ ///////////////
+function requestforrep() {
+
+    var y = document.getElementById("form1");
+    var x = document.getElementById("form2");
+
+    y.style.display = "block";
+    x.style.display = "none";
+
+}
+
+function borrow() {
+    var y = document.getElementById("form1");
+    var x = document.getElementById("form2");
+
+    y.style.display = "none";
+    x.style.display = "block";
+
+}
