a0089175d9de04e49fadc1f503b887f4df695558
M1xels <micoryan.sanpablo@gsfe.tupcavite.edu.ph>
2022-10-04 16:46:43 +0800

    yung register displaying inserted pic unting responsiveness pa

--- TupcOnlineSys/TupcSysApp/static/js/registration.js
+++ TupcOnlineSys/TupcSysApp/static/js/registration.js
@@ -1,18 +1,38 @@
-function toggle() {
+function next1() {
     var x = document.getElementById("form1");
     var y = document.getElementById("form2");
+    var Z = document.getElementById("form3");
     x.style.display = "none";
     y.style.display = "block";
+    Z.style.display = "none";
 
 }
 
-function toggle1() {
+function next2() {
     var x = document.getElementById("form1");
     var y = document.getElementById("form2");
-    x.style.display = "block";
-
-
+    var Z = document.getElementById("form3");
+    x.style.display = "none";
     y.style.display = "none";
+    Z.style.display = "block";
+
+}
+function back2() {
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var Z = document.getElementById("form3");
+    x.style.display = "none";
+    y.style.display = "block";
+    Z.style.display = "none";
+
+}
+function back1() {
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var Z = document.getElementById("form3");
+    x.style.display = "block";
+    y.style.display = "none";
+    Z.style.display = "none";
 
 }
 
