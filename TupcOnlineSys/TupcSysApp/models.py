50ae6e02de14ff06be5e3276f661ebe10fe057f4
mix st.paul <micoryan.sanpablo@gsfe.tupcavite.edu.ph>
2022-12-08 23:43:27 +0800

    lidno max length correction

--- TupcOnlineSys/TupcSysApp/models.py
+++ TupcOnlineSys/TupcSysApp/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import AbstractUser
 
 class list(models.Model):
     lgsfe = models.CharField(max_length= 100, null=False)
-    lidno = models.CharField(max_length= 20, null=False)
+    lidno = models.CharField(max_length= 50, null=False)
     type = models.CharField(max_length= 100, null=False)
 
 class register1(AbstractUser):
