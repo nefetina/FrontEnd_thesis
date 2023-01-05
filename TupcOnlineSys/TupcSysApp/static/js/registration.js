9c2f6a4f0a4256ff76a8608be617ec7f30588c57
DaniRamos28 <91649967+DaniRamos28@users.noreply.github.com>
2023-01-05 22:20:07 +0800

    Update registration.js

--- TupcOnlineSys/TupcSysApp/static/js/registration.js
+++ TupcOnlineSys/TupcSysApp/static/js/registration.js
@@ -1,10 +1,48 @@
+const e = require("express");
+
 function next1() {
-    var x = document.getElementById("form1");
-    var y = document.getElementById("form2");
-    var Z = document.getElementById("form3");
-    x.style.display = "none";
-    y.style.display = "block";
-    Z.style.display = "none";
+
+    var fgsfe = document.getElementById("id_gsfe").value;
+    var idnum = document.getElementById("id_username").value;
+    const gsfe = document.getElementsByName("gsfes");
+    const idno = document.getElementsByName("idnums");
+
+    for (let i = 0; i < idno.length; i++) {
+        var id = idno[i].value;
+        if (id == idnum){
+            var a = "authenticated";
+            var row = i;
+            break
+        }
+      }
+    for (let x = 0; x < gsfe.length; x++) {
+        var gs = gsfe[x].value;
+        
+        if (gs == fgsfe && id == idnum){
+            var b = "authenticated";
+            var row1 = x;
+            break
+        }
+    }
+    if (a == "authenticated" && b == "authenticated" && row == row1){
+
+        var x = document.getElementById("form1");
+        var y = document.getElementById("form2");
+        var Z = document.getElementById("form3");
+        x.style.display = "none";
+        y.style.display = "block";
+        Z.style.display = "none";
+    }
+    else if (fgsfe == "" || idnum == ""){
+        alert("Please fill up the form.")
+    }
+    else {
+        alert("Your GSFE Account or ID Number is not in the list, or please recheck all your input.");
+    }
+
+    
+
+
 
 }
 
